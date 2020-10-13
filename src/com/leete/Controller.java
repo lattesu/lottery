abb536c127f433b8eb8ebf5d7b6f18e1069fb145
latte <lattesu@126.com>
2020-10-13 17:22:42 +0800

    Initial commit

---
+++ src/com/leete/Controller.java
@@ -0,0 +1,4 @@
+package com.leete;
+
+public class Controller {
+}
---
+++ src/com/leete/Main.java
@@ -0,0 +1,23 @@
+package com.leete;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("lottery.fxml"));
+        primaryStage.setTitle("Lottery Ticket");
+        primaryStage.setScene(new Scene(root, 700, 900));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
